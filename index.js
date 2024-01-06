b94b28ba13db591eae2a7ed7b968a0a9499101a8
ammar arief <ammararief321@gmail.com>
2024-01-06 23:06:39 +0700

    edit port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT ;
 
 app.get('/loker', async (req, res) => {
   try {
