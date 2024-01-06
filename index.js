5ddc15ea390b8d89364cfcf32f513032e50ea7aa
ammar arief <ammararief321@gmail.com>
2024-01-06 23:10:34 +0700

    change port env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = 3000 || process.env.PORT ;
+const port =  process.env.PORT || 3000  ;
 
 app.get('/loker', async (req, res) => {
   try {
